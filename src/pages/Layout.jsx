2c0826379eccf1d47ae069669aa45cda9eb41178
Sahil-Coder1 <26maysahil@gmail.com>
2026-04-15 13:57:45 +0530

    responsiveness enhanced for mobile

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,22 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 function Layout() {
+  const location = useLocation();
+
   return (
-    <>
+    <div className="min-h-[100dvh] flex flex-col">
       <Header />
+
       {location.pathname !== "/" && (
         <div className="lg:px-4 px-0">
           <NavBar />
         </div>
       )}
-      <main className="min-h-fit lg:px-8 px-0">
+
+      <main className="flex-1 lg:px-8 px-4 py-4">
         <Outlet />
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
